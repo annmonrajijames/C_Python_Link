b35ef3a7554bd2ebdebdc0d5d5954286bc5dea0b
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-05-07 18:34:19 +0530

    11 inputs from Python file will be transferred to C file and then it will show the output in python

--- cfile.c
+++ cfile.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello\n");
+    int c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11;
+    scanf("%d %d %d %d %d %d %d %d %d %d %d", &c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8, &c9, &c10, &c11);
+    c1=999;
+    printf("%d %d %d %d %d %d %d %d %d %d %d\n", c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11);
     return 0;
 }
--- pfile.py
+++ pfile.py
@@ -1,1 +1,13 @@
-print("Hello")
+import subprocess
+
+def run_c_program():
+    a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11 = 1, 10, 7, 0, 6, 1, 22, 11, 23, 11, 99
+    input_values = f"{a1} {a2} {a3} {a4} {a5} {a6} {a7} {a8} {a9} {a10} {a11}"
+    
+    # Start the compiled C program and pass the formatted string of input values
+    process = subprocess.Popen(['./cfile'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate(input=input_values.encode())
+    return stdout.decode().strip()
+
+result = run_c_program()
+print("Output from C program:", result)
