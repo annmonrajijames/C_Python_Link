61b9873f8782666380da87964053e4a29000a4ba
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-05-08 22:09:27 +0530

    deleted all files/folders

--- cfile.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int c1, c2, c3, c4, c5, thr_per, c7, c8, c9, c10, c11;
-    scanf("%d %d %d %d %d %d %d %d %d %d %d", &c1, &c2, &c3, &c4, &c5, &thr_per, &c7, &c8, &c9, &c10, &c11);
-    printf("%d %d %d %d %d %d %d %d %d %d %d\n", c1, c2, c3, c4, c5, thr_per, c7, c8, c9, c10, c11);
-    return 0;
-}
